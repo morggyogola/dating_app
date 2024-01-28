0b3d172b598f1fb59311aaab67b3263c2a1dff9b
Morgan Ogola <morganogola@gmail.com>
2024-01-28 09:28:00 +0300

    first commit

---
+++ lib/screens/get_started.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class GetStarted extends StatelessWidget {
+  const GetStarted({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+
+    );
+  }
+}
+
